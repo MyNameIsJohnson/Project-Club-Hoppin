d32f00debe48de55b2a2c7891276e66ae7264a17
Johnson Le Thieu <johnsonle2929@gmail.com>
2020-02-29 17:38:39 -0800

    Revert "setup server"

--- models/Post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
- 
-const PostSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    reviews: String,
-});
-
-module.exports = mongoose.model('Post', PostSchema);
