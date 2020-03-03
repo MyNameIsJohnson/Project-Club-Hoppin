d44a63095d9b9ff92fc3463d11000e388077d1b1
Johnson Thieu <johnsonle2929@gmail.com>
2020-03-02 16:18:58 -0800

    fixed server.js removing second listen(PORT)

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
-// ----------- Root API Directory
+// // ----------- Root API Directory
 router.get('/', (req, res) => {
     console.log("beep")
    
 })
-// ----------- Clubs Routes
+// // ----------- Clubs Routes
 router.get('/clubs', ctrl.clubs.index);
 router.get('/clubs/:id', ctrl.clubs.show);
 router.post('/clubs', ctrl.clubs.create);
@@ -19,4 +19,4 @@ router.get('/posts', ctrl.posts.index);
 router.post('/clubs/:clubId/posts', ctrl.posts.create);
 router.delete('/clubs/:clubId/posts/:postId', ctrl.posts.destroy);
 
-module.exports = router
+module.exports = router;
