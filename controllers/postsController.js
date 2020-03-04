a42298f88477399ac0c4cfbe2d2fee7d2ad63117
MarkGee45 <moel55.mg@gmail.com>
2020-03-03 16:57:07 -0800

    changes

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -26,13 +26,5 @@
  module.exports = {
      index,
      create,
-//  const show = (req, res) => {
-//     db.Club.findById(req.params.id, (err, foundPosts) => {
-//         if (err) return res.status(400).json({status: 400, message: 'Something went wrong, please try again'}),
-//         res.json(foundPosts);
-        
-//     });
-//  };
- module.exports = {
-     index
+
  };
