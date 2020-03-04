f6945e78957815e71de1a01e142a947f52baba3f
MarkGee45 <moel55.mg@gmail.com>
2020-03-04 15:38:56 -0800

    added a create to post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -8,21 +8,25 @@
  };
 
  const create = (req, res) => {
-    db.Post.create(req.body, (err, foundPosts) => {
-        if (err) return res.status(400).json({status: 400, message: 'Something went wrong, please try again'}),
-        res.json(foundPosts);
-    });
-
-    // db.Club.findById(req.params.clubsID, (err, foundClub) => {
-    //     if (err) return res.status(400).json({ status: 400, message: 'Something went wrong, please try again'}),
-    //     res.json(foundClub);
-
-    // foundClub.posts.push(foundPosts);
-
-
-    // });
+   console.log(req.body)
+    db.Post.create(req.body, (err, post) => {
+        if (err) return res.status(400).json({status: 400, message: 'Something went wrong, please try again'});
+        console.log('beep')
+ db.Club.findById(req.params.clubId, (err, foundClub) => {
+    if (err) return res.status(400).json({ status: 400, message: 'Something went wrong, please try again'});
     
+    foundClub.reviews.push(post);
+    console.log(foundClub)
+    //foundClub.save()
+    
+    res.json(foundClub);
+});
+    
+
+});
+
  };
+ 
  module.exports = {
      index,
      create,
