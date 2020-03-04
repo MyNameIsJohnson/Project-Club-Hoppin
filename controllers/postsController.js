683bedafb54bf79697bc7aa5c2c9e109d2be3d8b
MarkGee45 <moel55.mg@gmail.com>
2020-03-03 16:49:02 -0800

    testing posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -7,8 +7,6 @@
      });
  };
 
-//  const create = (req, res) => {
-//     db.Post.create(req.body, (err, foundPosts) => {
  const create = (req, res) => {
     db.Post.create(req.body, (err, foundPosts) => {
         if (err) return res.status(400).json({status: 400, message: 'Something went wrong, please try again'}),
@@ -28,23 +26,4 @@
  module.exports = {
      index,
      create,
-//  const show = (req, res) => {
-//     db.Club.findById(req.params.id, (err, foundPosts) => {
-//         if (err) return res.status(400).json({status: 400, message: 'Something went wrong, please try again'}),
-//         res.json(foundPosts);
-        
-//     db.Club.findById(req.params.clubsID, (err, foundClub) => {
-//         if (err) return res.status(400).json({ status: 400, message: 'Something went wrong, please try again'}),
-//         res.json(foundClub);
-
-//     foundClub.posts.push(foundPosts);
-
-
-//     });
-    
-//     })
-//  };
- module.exports = {
-     index,
-     //create,
- };
+ }
