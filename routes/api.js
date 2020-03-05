616af15561889e7faa1503fed16ccca074c03689
MarkGee45 <moel55.mg@gmail.com>
2020-03-05 11:33:38 -0800

    deleted some comments

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,6 @@ const ctrl = require('../controllers');
 // ----------- Clubs Routes
 router.get('/clubs', ctrl.clubs.index);
 router.get('/clubs/:clubId', ctrl.clubs.show);
-// router.post('/clubs', ctrl.clubs.create);
-// router.delete('/clubs/:id', ctrl.clubs.destroy);
 
 // ------------- Post Routes
 router.get('/posts', ctrl.posts.index);
