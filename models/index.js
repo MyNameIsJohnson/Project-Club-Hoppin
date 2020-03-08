ac5361273058c9511ed575f3141b997c610efe40
Johnson Thieu <johnsonle2929@gmail.com>
2020-03-07 18:09:27 -0800

    uri changes

--- models/index.js
+++ models/index.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Project1Club'
-mongoose.connect(DB_URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => console.log('MongoDB is connected'))
-    .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Project1Club';
   
     module.exports = {
       Post: require('./Post.js'), 
