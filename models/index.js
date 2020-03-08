64ad110bcc22f551b1a41c3e49d3c43525aebce7
Johnson Thieu <johnsonle2929@gmail.com>
2020-03-07 18:10:48 -0800

    uri changes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Project1Club'
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Project1Club';
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Project1Club');
   
     module.exports = {
       Post: require('./Post.js'), 
