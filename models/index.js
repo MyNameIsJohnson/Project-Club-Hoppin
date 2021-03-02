42165480183a82df895b8f93d1255bd6ab89b061
MyNameIsJohnson <johnsonle2929@gmail.com>
2021-03-01 17:38:20 -0800

    heroku test3

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URI = process.env.MONGODB_URI || 'mongodb+srv://johnsonthieu:Mymongodb929!@sei.galny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const DB_URI = process.env.MONGODB_URI || 'mongodb+srv://johnsonthieu:Mymongodb929!@sei.galny.mongodb.net/Project-1-Club-Hoppin?retryWrites=true&w=majority'
 mongoose.connect(DB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false,
