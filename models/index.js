da772f4af7559db3e77ba453829d0efd7d62faf0
Johnson Thieu <johnsonthieu@Johnsons-MacBook-Air.local>
2021-08-17 15:59:39 -0700

    ready for heroku deploy attempt #1

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Project1Club';
-mongoose.connect(DB_URI, {
+// const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Project1Club';
+const MONGODB_URI='mongodb+srv://johnsonthieu:Mymongodb929!@club.ztdyc.mongodb.net/club?retryWrites=true&w=majority';
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
