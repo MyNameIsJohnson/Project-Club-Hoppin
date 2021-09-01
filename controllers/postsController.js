5233a6d0e8101a9f1563ac87ba18a0a635740f35
johnsonthieu <johnsonle2929@gmail.com>
2021-09-01 16:48:40 -0700

    Adds newPost

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -19,21 +19,23 @@
 // ---------------- Creating A Review (Post)
  const create = (req, res) => {
    console.log(req.body)
-    db.Post.create(req.body, (err, post) => {
+    
+  db.Post.create(req.body, (err, newPost) => {
         if (err) return res.status(400).json({status: 400, message: 'Something went wrong, please try again'});
        
- db.Club.findById(req.params.clubId, (err, foundClub) => {
-    if (err) return res.status(400).json({ status: 400, message: 'Something went wrong, please try again'});
-    
-    foundClub.reviews.push(post);
-    
-    foundClub.save((err, savedPost) => {
+    db.Club.findById(req.params.clubId, (err, foundClub) => {
+      if (err) return res.status(400).json({ status: 400, message: 'Something went wrong, please try again'});
+      
+      foundClub.posts.push(newPost);
+      
+      foundClub.save((err, savedPost) => {
         if (err) return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
-    });
     
-    res.json(foundClub);
-});
-});
+    
+        res.json(newPost);
+      });
+    });
+  });
 };
 // ------------ Get And Edit A Review (Post)
 const update = (req, res) => {
@@ -44,7 +46,7 @@ const update = (req, res) => {
       db.Club.findById(req.params.clubId, (err, foundClub) => {
         if (err) return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
     
-        const updatePost = foundClub.reviews.id(req.params.postId);
+        const updatePost = foundClub.posts.id(req.params.postId);
         updatePost.set(req.body)
         foundClub.save((err, savedClub) => {
          if (err) return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
@@ -61,20 +63,19 @@ const destroy = (req, res) => {
     db.Club.findById(req.params.clubId, (err, foundClubs) => {
     if (err) return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
 
-    const deletePost = foundClubs.reviews.id(req.params.postId);
+    const deletePost = foundClubs.posts.id(req.params.postId);
 
     if (!deletePost) {
         return res.status(400).json({status: 400, error: 'Could not find post'});
+    }
+    deletePost.remove();
 
-    
-}
-deletePost.remove();
-
-foundClubs.save((err, savedClubs) => {
+  foundClubs.save((err, savedClubs) => {
     if (err) return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
+    
     db.Post.findByIdAndDelete(req.params.postId, (err, deletedPost) => {
         if (err)
-            return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
+          return res.status(400).json({status: 400, error: 'Something went wrong, please try again'});
 
           res.json(deletedPost);
       });
